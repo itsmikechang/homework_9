654b9232fb38ce5291d6908621cc7fde598a35d9
itsmikechang <msaechang1@gmail.com>
2020-08-30 22:20:41 -0700

    master

--- apiRoutes.js
+++ apiRoutes.js
@@ -1,33 +1,32 @@
-const fs = require('fs');
-const store = require("./db/db.json");
+const fs = require("fs");
+const content = require("../db/db.json");
 
-module.exports = function (app) {
+module.exports = function(app) {
+    
     app.get('/api/notes', function(req, res) {
-        fs.readFile('./db/db.json', 'utf8', function (err, data){
-            if (err) {
-                console.log(err);
-            }
-            store = JSON.parse(data);
-            res.json(notes);
-        });
-    });
+        res.json(content);
+      });
+    
+    app.post('/api/notes', function(req, res) {
+        let newContent = req.body;
+        let newId = content.length
+        newContent.id = newId;
+        content.push(newContent);
 
-    app.post('./api/notes', function(req,res) {
-        let newStore = req.body;
-        let newId = store.length
-        newStore.id = newId;
-        store.push(newStore);
-
-        fs.writeFilesSync("./db/db.json", JSON.stringify(content), function(err) {
+        fs.writeFileSync("./db/db.json", JSON.stringify(content), function(err) {
             if (err) throw (err);
         });
+
+        res.json("ok");
     });
- 
-    app.delete('./api/notes/:id', function(req, res) {
-        let deleteNote = store.find(({id}) => id === JSON.parse(req.params.id));
-        store.splice(store.indexOf(deleteNote), 1);
-        fs.writeFile("./db/db.json", JSON.stringify(store), function(err) {
+
+    app.delete('/api/notes/:id', function(req, res) {
+        let deleteContent = content.find(({id}) => id === JSON.parse(req.params.id));
+        content.splice(content.indexOf(deleteContent), 1);
+        fs.writeFile("./db/db.json", JSON.stringify(content), function(err) {
             if (err) throw (err);
+            res.json("Note Deleted");
         });
     });
-};
+    
+    }

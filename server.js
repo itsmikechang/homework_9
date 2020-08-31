493afbd5ef47734ae25f5dc5a104114e7ccc32ed
itsmikechang <msaechang1@gmail.com>
2020-08-30 22:18:12 -0700

    forgot some stuff

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-require("./apiRoutes")(app);
-require("./htmlRoutes")(app);
+require("./apiRoutes.js")(app);
+require("./htmlRoutes.js")(app);
 
 app.listen(PORT,function() {
     console.log('App listening on Port: ' + PORT);
